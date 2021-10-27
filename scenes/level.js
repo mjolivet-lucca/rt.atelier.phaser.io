c6b0fe36603329ee2dc546cc8d911effc61c0e4e
Benjamin <bsabardu@lucca.fr>
2021-10-27 16:39:41 +0200

    Add bonus & gournd

--- scenes/level.js
+++ scenes/level.js
@@ -10,20 +10,24 @@ var Level = new Phaser.Class({
     init: function (){
         cursors = this.input.keyboard.createCursorKeys();
         jumpButton = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        jumpTimer = null;
     },
 
     preload: function (){},
 
     create: function (){
         this.player = this.physics.add.sprite(this.cameras.main.width / 2, 0, 'player');
+        this.bonus = this.physics.add.sprite(600,0,'armor');
         this.physics.world.setBounds(0, 0, 1000, 300);
-        this.cameras.main.setBounds(0, 0, 400, 300);
+        this.cameras.main.startFollow(this.player);
+        this.cameras.main.setBounds(0, 0, 1000, 300);
 
         this.player.setCollideWorldBounds(true);
 
         let platform = this.physics.add.staticGroup();
-        platform.create(this.cameras.main.width / 2, 300, "ground");
+        platform.create(this.cameras.main.width / 2, 300, "ground")
         this.physics.add.collider(this.player, platform);
+        this.physics.add.collider(this.bonus,platform);
     },
 
     update: function (){
@@ -33,8 +37,13 @@ var Level = new Phaser.Class({
         if(cursors.right.isDown){
             this.player.body.velocity.x = 50;
         }
-        if(jumpButton.isDown){
-            this.player.body.velocity.y = -50;
+        if (jumpButton.isDown && this.player.body.onFloor() && this.time.now > jumpTimer)
+        {
+            this.player.body.velocity.y = -150;
+            jumpTimer = this.time.now + 750;
         }
+        if(cursors.left.isUp && cursors.right.isUp){
+            this.player.body.velocity.x = 0
+        };
     },
 });
